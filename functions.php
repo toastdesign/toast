aeb20e405adf445fb53ec407b241195ff610b864
toastdesign <info@toastdesign.nl>
2013-04-13 13:46:49 +0200

    Plugin activation function added

--- functions.php
+++ functions.php
@@ -15,4 +15,5 @@ require_once locate_template('/lib/htaccess.php');        // HTML5 Boilerplate .
 require_once locate_template('/lib/widgets.php');         // Sidebars and widgets
 require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
 require_once locate_template('/lib/shortcodes.php');      // Custom shortcodes
+require_once locate_template('/lib/pluginactivation.php');// Plugin activation
 require_once locate_template('/lib/custom.php');          // Custom functions
