35d0f01fa283b9ba8de100000fdd1eba9184e674
toastdesign <info@toastdesign.nl>
2013-04-16 21:43:16 +0200

    Deleted some files

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <?php endif; ?>
 
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', get_post_format()); ?>
+  <?php get_template_part('templates/post-format/content', get_post_format()); ?>
 <?php endwhile; ?>
 
 <?php if ($wp_query->max_num_pages > 1) : ?>
--- single.php
+++ single.php
@@ -1,1 +1,1 @@
-<?php get_template_part('templates/content', 'single'); ?>
+<?php get_template_part('templates/post-format/single', get_post_format()); ?>
