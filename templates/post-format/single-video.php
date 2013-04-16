5c746eb3ff25ba2457bab44945f910068ee308bc
toastdesign <info@toastdesign.nl>
2013-04-16 22:16:19 +0200

    Even cleaner bij deleting

--- templates/post-format/single-video.php
+++ templates/post-format/single-video.php
@@ -1,20 +1,13 @@
-<div class="post clearfix">
+<?php while (have_posts()) : the_post(); ?>
+<article <?php post_class(); ?>>
+    
+    <header>
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+    </header>
+    
+    <div class="entry-content">
+        <?php the_content(); ?>
+    </div>
 
-	<div class="post-video">
-		<?php echo get_post_meta($post->ID, '_format_video_embed', true); ?>
-	</div>
-	
-	<a href="<?php echo get_post_format_link('video'); ?>" class="post-icon video"></a>
-	
-	<div class="post-content">
-		<div class="post-title">
-			<h2><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__('Permalink to %s', 'minti'), the_title_attribute('echo=0') ); ?>" rel="bookmark">
-				<?php the_title(); ?>
-			</a></h2>
-		</div>
-		<div class="post-meta"><?php get_template_part( 'framework/inc/meta' ); ?></div>
-		<div class="post-excerpt"><?php the_content(); ?></div>		
-		<div class="post-tags clearfix"><?php the_tags( '', '', ''); ?></div>
-	</div>
-
-</div>
+</article>
+<?php endwhile; ?>
--- templates/post-format/single.php
+++ templates/post-format/single.php
@@ -1,14 +1,13 @@
 <?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class(); ?>>
+<article <?php post_class(); ?>>
+    
     <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
+        <h1 class="entry-title"><?php the_title(); ?></h1>
     </header>
+    
     <div class="entry-content">
-      <?php the_content(); ?>
+        <?php the_content(); ?>
     </div>
-    <footer>
-      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
-      <?php the_tags('<ul class="entry-tags"><li>','</li><li>','</li></ul>'); ?>
-    </footer>
-  </article>
+    
+</article>
 <?php endwhile; ?>
